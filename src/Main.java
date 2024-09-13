fc54dafb9eb90b4a6e57e708a1163e92ecfe8394
Afonso B. Sousa <11749310+abtsousa@users.noreply.github.com>
2024-09-14 00:27:24 +0100

    Typo fix.

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static final String REMOVE_CONTACT = "RC";
     public static final String GET_PHONE      = "GP";
     public static final String GET_EMAIL      = "GE";
-    public static final String GET_NUMBER     = "GN";
+    public static final String GET_NAME     = "GN";
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
@@ -23,7 +23,7 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
-    public static final String GET_NUMBER_MSG = "Phone number does not exist.";
+    public static final String GET_NAME_MSG = "Phone number does not exist.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
